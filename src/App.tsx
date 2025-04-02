65d027ecae228345763256f3ee5f159cebb78f07
Bradley <104906904+bradley-03@users.noreply.github.com>
2025-04-02 13:32:22 +0100

    feat(wip): created random country roll

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
+import countryData from "../data/countryData.json"
+import { useState } from "react"
+
+function getRandomCountryIndex(): number {
+  return Math.floor(Math.random() * countryData.length)
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [randomCountryIndex, setRandomCountryIndex] = useState<number>(getRandomCountryIndex())
+
+  function rollNewCountry() {
+    setRandomCountryIndex(getRandomCountryIndex())
+  }
+
+  const country = countryData[randomCountryIndex]
 
   return (
     <>
+      <button onClick={rollNewCountry}>New Country</button>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>
+          {country.name.common} {country.flag}
+        </h1>
+        <img src={country.flags.png} alt={country.flags.alt} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
