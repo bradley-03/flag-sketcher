b7b12de72b639e7e3e085feb3a995521a194017f
Bradley <104906904+bradley-03@users.noreply.github.com>
2025-04-03 14:35:37 +0100

    feat: added extra controls to the canvas

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -5,6 +5,7 @@ export default function Canvas() {
   const canvasRef = useRef<ReactSketchCanvasRef>(null)
   const [eraseMode, setEraseMode] = useState<boolean>(false)
   const [strokeColor, setStrokeColor] = useState<string>("black")
+  const [strokeWidth, setStrokeWidth] = useState<number>(3)
 
   const handleEraserClick = () => {
     setEraseMode(true)
@@ -20,19 +21,41 @@ export default function Canvas() {
     setStrokeColor(event.target.value)
   }
 
+  function handleUndoClick() {
+    canvasRef.current?.undo()
+  }
+
+  function handleRedoClick() {
+    canvasRef.current?.redo()
+  }
+
+  function handleClearClick() {
+    canvasRef.current?.resetCanvas()
+  }
+
+  function handleStrokeWidthChange(event: ChangeEvent<HTMLInputElement>) {
+    setStrokeWidth(+event.target.value)
+  }
+
   return (
     <div>
+      <input type="color" onChange={updateStrokeColor} />
       <button onClick={handlePenClick} disabled={!eraseMode}>
         Pen
       </button>
       <button onClick={handleEraserClick} disabled={eraseMode}>
         Eraser
       </button>
-      <input type="color" onChange={updateStrokeColor} />
+      <input type="range" onChange={handleStrokeWidthChange} value={strokeWidth} min={1} max={100} />
+      <button onClick={handleUndoClick}>Undo</button>
+      <button onClick={handleRedoClick}>Redo</button>
+      <button onClick={handleClearClick}>Clear</button>
+
       <ReactSketchCanvas
         ref={canvasRef}
         strokeColor={strokeColor}
         canvasColor="white"
+        strokeWidth={strokeWidth}
         eraserWidth={30}
         height="250px"
         width="25%"
