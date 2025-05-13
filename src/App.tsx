11c66d4a180dfbb2b8e00bcf031a4b575e4a1bfb
Bradley <104906904+bradley-03@users.noreply.github.com>
2025-05-13 16:51:31 +0100

    feat: added game history state tracking and visual history display

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,15 @@ import { CanvasRefHandle } from "./components/DrawingCanvas/CanvasCore"
 import Button from "./components/Button"
 import { compareImages, getImageAspectRatio } from "./util/compareImages"
 
+type GameHistoryState = {
+  country: {
+    name: string
+    flag: string
+  }
+  userDrawing: string
+  accuracy: number
+}[]
+
 function getRandomCountryIndex(): number {
   return Math.floor(Math.random() * countryData.length)
 }
@@ -13,6 +22,7 @@ function App() {
   const [randomCountryIndex, setRandomCountryIndex] = useState<number>(getRandomCountryIndex())
   const [flagAspect, setFlagAspect] = useState<number>(3 / 2)
   const canvasRef = useRef<CanvasRefHandle>(null)
+  const [gameHistory, setGameHistory] = useState<GameHistoryState>([])
 
   async function rollNewCountry() {
     const newCountryIndex = getRandomCountryIndex()
@@ -23,10 +33,21 @@ function App() {
 
   const country = countryData[randomCountryIndex]
 
-  function exportTest() {
+  async function exportTest() {
     const imgBase64 = canvasRef.current?.exportImg()
     if (!imgBase64) return
-    compareImages(imgBase64, country.flags.png)
+    const { accuracy } = await compareImages(imgBase64, country.flags.png)
+    setGameHistory(prev => [
+      ...prev,
+      {
+        country: {
+          name: country.name.common,
+          flag: country.flags.png,
+        },
+        userDrawing: imgBase64,
+        accuracy,
+      },
+    ])
   }
 
   // Calculate new aspect ratio when the flag changes
@@ -46,6 +67,20 @@ function App() {
 
         <Button onClick={exportTest}>Test Button</Button>
         <DrawingCanvas ref={canvasRef} aspectRatio={flagAspect} />
+      </div>
+
+      <div className="flex flex-col w-full justify-center items-center gap-3 mt-5">
+        <h2>Game History</h2>
+        {gameHistory.map((game, index) => (
+          <div key={index} className="flex flex-col items-center">
+            <h2 className="font-bold">{game.country.name}</h2>
+            <p className="font-semibold">Accuracy: {game.accuracy.toFixed(2)}%</p>
+            <div className="flex flex-row gap-2">
+              <img src={game.country.flag} alt={`${game.country.name} flag`} />
+              <img src={game.userDrawing} alt={`${game.country.name} flag`} className="w-[20rem]" />
+            </div>
+          </div>
+        ))}
       </div>
     </>
   )
